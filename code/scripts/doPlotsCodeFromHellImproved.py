4dcd1895f64ce6d3b03d42d8c5005d208fc46568
Joaquin Rapela <joacorapela@gmail.com>
2021-05-15 18:30:49 +0100

    Plotting two panels

--- code/scripts/doPlotsCodeFromHellImproved.py
+++ code/scripts/doPlotsCodeFromHellImproved.py
@@ -7,15 +7,24 @@ import plotting
 
 def main(argv):
     dataFilename = "../../data/All_three_exp_conditions_4.csv"
-    figFilename = "../../figures/spikeRateVsabsSpeedV1_allConditions.png"
+    figFilename = "../../figures/spikeRateVsabsSpeedV1RSPg_allConditions.png"
     data = pd.read_csv(dataFilename, index_col=0)
 
-    fig, axs = plt.subplots(3, 1)
+    fig, axs = plt.subplots(3, 2)
 
-    plotting.plot_panel(condition="Visual", data=data, ax=axs[0])
-    plotting.plot_panel(condition="Vestibular", data=data, ax=axs[1])
-    plotting.plot_panel(condition="VisVes", data=data, ax=axs[2],
-                        title="Visual + Vestibular")
+    plotting.plot_panel(condition="Visual", region="V1", data=data,
+                        ax=axs[0, 0])
+    plotting.plot_panel(condition="Vestibular", region="V1", data=data,
+                        ax=axs[1, 0])
+    plotting.plot_panel(condition="VisVes", region="V1", data=data,
+                        ax=axs[2, 0], title="V1: Visual + Vestibular")
+
+    plotting.plot_panel(condition="Visual", region="RSPg", data=data,
+                        ax=axs[0, 1])
+    plotting.plot_panel(condition="Vestibular", region="RSPg", data=data,
+                        ax=axs[1, 1])
+    plotting.plot_panel(condition="VisVes", region="RSPg", data=data,
+                        ax=axs[2, 1], title="V1: Visual + Vestibular")
 
     plt.savefig(figFilename)
 
